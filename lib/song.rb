cc6c0e5884be78e231cbf151c66a48a744415026
Mark Viti <+>
2022-06-01 10:43:41 -0400

    commit tree 583094eaf890326245cde9cd86e648dd21ec7cdc

--- lib/song.rb
+++ lib/song.rb
@@ -1,6 +1,11 @@
+require 'pry'
 class Song
   attr_accessor :name, :artist_name
   @@all = []
+
+  def name
+    @name
+  end
 
   def self.all
     @@all
@@ -10,4 +15,59 @@ class Song
     self.class.all << self
   end
 
+  def self.create
+    song = Song.new
+    song.save
+    return song
+  end
+
+  def self.new_by_name(name)
+    song = Song.new
+    song.name= name
+    return song
+  end
+
+  def self.create_by_name(name)
+    song = Song.new
+    song.name= name
+    song.save
+    return song
+  end
+  
+  def self.find_by_name(name)
+    @@all.find{|song| song.name == name}
+  end 
+
+  def self.find_or_create_by_name(name)
+    if self.find_by_name(name) == nil 
+      self.create_by_name(name)
+    else 
+      return self.find_by_name(name)
+    end 
+  end 
+
+  def self.alphabetical 
+    @@all.sort_by { |obj| obj.name}
+  end
+
+  def self.new_from_filename(file)
+    file.slice! ".mp3"
+    a = file.split(/ - /)
+    song = self.new_by_name(a[1])
+    song.artist_name= a[0]
+    return song
+  end
+
+  def self.create_from_filename(file)
+    file.slice! ".mp3"
+    a = file.split(/ - /)
+    song = self.create_by_name(a[1])
+    song.artist_name= a[0]
+    return song
+  end
+
+
+  def self.destroy_all
+    @@all = []
+  end
 end
